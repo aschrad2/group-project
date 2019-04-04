a6511eed115cff15a01e1bf77876c6eb6b4b60ba
Alex Sasser and Rohan Straker <alex+rohan@>
2019-04-03 19:58:47 -0700

    tenative final draft.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,92 @@
   $(document).ready(function() {
+          $("#reload").click(function(){
+            location.reload(true);
+          });
           $("button").click(function(){
               event.preventDefault();
+
+              $("#output").show();
 
               var selectedAmenities = [];
               $.each($("input[name='park-item']:checked"), function(){
                   selectedAmenities.push($(this).val());
 
               });
-              if (selectedAmenities.includes("swing")) {
-                $("#creston").show();
+              if (selectedAmenities.includes("playground")) {
                 $("#fields").show();
+                $("#lovejoy").show();
+                $("#couch").show();
+                $("#elizabeth").show();
+                $("#sellwood").show();
+                $("#clackamas").show();
+              }
+
+              if (selectedAmenities.includes("swing")) {
+                $("#fields").show();
+                $("#lovejoy").show();
+                $("#couch").show();
+                $("#elizabeth").show();
+                $("#sellwood").show();
+                $("#creston").show();
+                $("#clackamas").show();
+              }
+
+              if (selectedAmenities.includes("picnic")) {
+                $("#fields").show();
+                $("#lovejoy").show();
+                $("#couch").show();
+                $("#elizabeth").show();
+                $("#creston").show();
+                $("#clackamas").show();
+              }
+
+              if (selectedAmenities.includes("baseball")) {
+                $("#fields").show();
+                $("#lovejoy").show();
+                $("#elizabeth").show();
+                $("#sellwood").show();
+                $("#creston").show();
+                $("#clackamas").show();
+              }
+
+              if (selectedAmenities.includes("basketball")) {
+                $("#fields").show();
+                $("#lovejoy").show();
+                $("#couch").show();
+                $("#elizabeth").show();
+                $("#sellwood").show();
+                $("#creston").show();
+                $("#clackamas").show();
+              }
+
+              if (selectedAmenities.includes("tennis")) {
+                $("#fields").show();
+                $("#couch").show();
+                $("#elizabeth").show();
+                $("#sellwood").show();
+                $("#creston").show();
+                $("#clackamas").show();
               }
 
               if (selectedAmenities.includes("soccer")) {
+                $("#lovejoy").show();
+                $("#couch").show();
+                $("#elizabeth").show();
+                $("#sellwood").show();
+                $("#creston").show();
                 $("#clackamas").show();
-
               }
-              debugger;
+
+              if (selectedAmenities.includes("discgolf")) {
+                $("#pier").show();
+              }
+
+              if (selectedAmenities.includes("skatepark")) {
+                $("#burnside").show();
+                $("#pier").show();
+              }
+
+
               //
               // if (selectedAmenities.includes("picnic")) {
               //   alert("true");
